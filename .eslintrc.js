ce3d4bb6e7e6acd77eed9b262d3ac5db8c042990
Benoit Mariat <benoit.mariat@intersec.com>
2021-09-18 11:35:30 +0200

    Change project style.
    Apply a more development style which is closer what I am used to.
    
    Mainly: 4 spaces indent, and semi-column at end of lines

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,52 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended'
+    ],
+    'rules': {
+        'block-scoped-var': 'error',
+        'camelcase': 'error',
+        'comma-dangle': ['error', 'always-multiline'],
+        'curly': 'error',
+        'default-case-last': 'error',
+        'eol-last': ['error', 'always'],
+        'eqeqeq': 'error',
+        'guard-for-in': 'error',
+        'linebreak-style': 'error',
+        'max-len': ['error', { 'code': 120 }],
+        'no-alert': 'error',
+        'no-bitwise': 'error',
+        'no-caller': 'error',
+        'no-cond-assign': 'error',
+        'no-debugger': 'error',
+        'no-empty-pattern': 'error',
+        'no-eval': 'error',
+        'no-extra-bind': 'error',
+        'no-extra-boolean-cast': 'error',
+        'no-extra-semi': 'error',
+        'no-floating-decimal': 'error',
+        'no-implied-eval': 'error',
+        'no-inner-declarations': 'error',
+        'no-multi-spaces': 'error',
+        'no-multi-str': 'error',
+        'no-octal': 'error',
+        'no-unexpected-multiline': 'off',
+        'no-unreachable': 'error',
+        'no-useless-rename': 'error',
+        'no-console': 'error',
+        'quotes': ['error', 'single', { 'allowTemplateLiterals': true }],
+        'semi': ['error', 'always'],
+    },
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
 </template>
 
 <style>
+:root {
+    --brand-primary: #42b983;
+    --text-color: #2c3e50;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: var(--text-color);
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: var(--text-color);
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: var(--brand-primary);
 }
 </style>
